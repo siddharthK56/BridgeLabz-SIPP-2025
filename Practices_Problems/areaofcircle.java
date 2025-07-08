2a9d0bd882abbf9f4cd17dfb8b06cc5d3c64573a
SiddharthK56 <siddharth.jdn@gmail.com>
2025-07-08 10:17:39 +0530

    delete all files

--- Practices_Problems/areaofcircle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class areaofcircle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float b = sc.nextFloat();
-        float area = (float) (3.14*b*b);
-        System.out.print(area);
-    }
-}
